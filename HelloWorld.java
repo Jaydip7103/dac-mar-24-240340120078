15a0cca95717debbca20de0faf4bf21579e62acf
Jaydip7103 <jaydipkadam7103@gmail.com>
2024-05-28 17:51:58 +0530

    Merge code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 class HelloWorld{
 public static void main(String[] args){
 System.out.println("Hello world welcome to java git");
+System.out.println("Hello world in branch 1");
+System.out.println("Hello world in branch 2");
+
 }
 
 }
